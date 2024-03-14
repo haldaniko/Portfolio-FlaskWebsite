fa424760d4242a55ea1a4e9baef9a0c31b10d73c
haldaniko <haldaniko@gmail.com>
2024-03-14 15:56:40 +0100

    New version 1.0

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
@@ -9,9 +9,37 @@ def home():
 
 
 @app.route("/portfolio/<project_number>")
-def home2(project_number):
+def project(project_number):
     return render_template(f"portfolio/{project_number}.html")
 
 
-if __name__ == "__main__":
+@app.route('/halytskyi_cv')
+def download_cv():
+    return send_file('static/my_files/Halytskyi_cv.pdf', mimetype='application/pdf')
+
+
+@app.route('/German_B2')
+def german():
+    return send_file('static/my_files/Certificate_B2.pdf', mimetype='application/pdf')
+
+
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        with open('form_data.txt', 'a', encoding='utf-8') as file:
+            file.write(f'Name: {name}, Email: {email}, Message: {message}\n')
+        file.close()
+
+        return render_template("index.html")
+
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -51,10 +51,10 @@ $(document).ready(function () {
         });
     }
 
-    // PORTFOLIO CONTENT  
-    $('.grid-container').cubeportfolio({
+    // PORTFOLIO CONTENT
+    $('.portfolio').cubeportfolio({
         layoutMode: 'grid',
-        filters: '.filters-container',
+        filters: '.portfolio-filter',
         gridAdjustment: 'responsive',
         animationType: 'skew',
         defaultFilter: '*',
@@ -102,7 +102,60 @@ $(document).ready(function () {
                     loadItems: 3,
                 }
             }
-    }); 
+    });
+
+    // CERTIFICATES CONTENT
+    $('.certificates').cubeportfolio({
+        layoutMode: 'grid',
+        filters: '.certificates-filter',
+        gridAdjustment: 'responsive',
+        animationType: 'skew',
+        defaultFilter: '*',
+        gapVertical: 0,
+        gapHorizontal: 0,
+        singlePageAnimation: 'fade',
+        mediaQueries: [{
+                width: 700,
+                cols: 3,
+            }, {
+                width: 480,
+                cols: 2,
+                options: {
+                    caption: '',
+                    gapHorizontal: 30,
+                    gapVertical: 20,
+                }
+            }, {
+                width: 320,
+                cols: 1,
+                options: {
+                    caption: '',
+                    gapHorizontal: 50,
+                }
+            }],
+        singlePageCallback: function (url, element) {
+            var t = this;
+            $.ajax({
+                    url: url,
+                    type: 'GET',
+                    dataType: 'html',
+                    timeout: 30000
+                })
+                .done(function (result) {
+                    t.updateSinglePage(result);
+                })
+                .fail(function () {
+                    t.updateSinglePage('AJAX Error! Please refresh the page!');
+                });
+        },
+            plugins: {
+                loadMore: {
+                    element: '#js-loadMore-agency',
+                    action: 'click',
+                    loadItems: 3,
+                }
+            }
+    });
 
     // BLOG CONTENT  
     $('#grid-blog').cubeportfolio({
