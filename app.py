d01b24a6454e4628ecce8040ea670297dab97dd4
haldaniko <haldaniko@gmail.com>
2024-09-26 16:35:35 +0200

    delete unnecessary comments

--- app.py
+++ app.py
@@ -37,9 +37,5 @@ def submit_form():
         return render_template("index.html")
 
 
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
-
 if __name__ == '__main__':
     app.run(debug=True)
